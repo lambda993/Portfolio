b0316c6e7ec68286b93c9bd1cb31793c40d11aab
Leopold Bjedov <leopold.bjedov@gmail.com>
2023-01-25 23:48:15 +0100

    Restyled dropdown menu button

--- script/index.js
+++ script/index.js
@@ -2,3 +2,5 @@ function extendNavbar() {
     items = document.getElementById('nav-items')
     items.classList.toggle('extend-navbar')
 }
+
+document.getElementById('dropdown-button').addEventListener('click', extendNavbar)
